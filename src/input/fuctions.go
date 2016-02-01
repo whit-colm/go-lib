cba37117f6fc2cc501dfca1d11e596520aed4aac
Whitman Huntley <huntleywhitman@gmail.com>
2016-02-01 15:17:32 -0500

    save

--- src/input/fuctions.go
+++ src/input/fuctions.go
@@ -5,28 +5,68 @@ import (
 	"fmt"
 	c "github.com/skilstak/go/colors"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func Prompt(p string) (t string, err error) {
 	//P in Prompt is capitalised because it's a public function,
 	//meaning it can be used in any go file if you know where to look.
-	fmt.Print(c.G + p)
+	//
+	//This is still here because a lot of my crap is dependant on this.
+	fmt.Print(c.X + p)
 	reader := bufio.NewReader(os.Stdin)
 	t, err = reader.ReadString('\n')
-	if err != nil {
-		return t, err
-	}
 	t = strings.TrimSpace(t)
 	return t, nil
 }
 
-/*
-func Ask(q string) string {
-	t, err := Prompt(q)
+func StringInput(p string) string {
+	//This function just returns whatever is typed as a string.
+	fmt.Print(c.X + p)
+	reader := bufio.NewReader(os.Stdin)
+	t, err = reader.ReadString('\n')
 	if err != nil {
-		return ""
+		fmt.Println(err)
+		os.Exit(-1)
 	}
+	t = strings.TrimSpace(t)
+	t = strings.ToLower(t)
 	return t
 }
-*/
+
+func IntInput(p string) int {
+	//This function just returns whatever is typed as a string.
+	fmt.Print(c.X + p)
+	reader := bufio.NewReader(os.Stdin)
+	t, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	t = strings.TrimSpace(t)
+	i, err := strconv.Atoi(t)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	return i
+}
+
+func DoubleInput(p string) Int {
+	//This function just returns whatever is typed as a string.
+	fmt.Print(c.X + p)
+	reader := bufio.NewReader(os.Stdin)
+	t, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	t = strings.TrimSpace(t)
+	f, err := strconv.ParseFloat(t, 64)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	return f
+}
---
+++ src/solver/module.go
@@ -0,0 +1,22 @@
+package nil
+
+import(
+	"fmt"
+	c "github.com/skilstak/go/colors"
+	i "input"
+	"strings"
+	"other"
+)
+
+//////////////////////////////////////////////////
+//This is just a hub for each of the modules//////
+//The real programming code is in another castle//
+//////////////////////////////////////////////////
+
+
+func Startup(){
+	//the only function in this
+	isDone := false
+	for isDone == false{
+		//So that you don't have to restart the program each time
+	}
