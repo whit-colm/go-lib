b5ef92170320b7f7bcd6757c3bcfeaf783f8b80c
Whitman Huntley <huntleywhitman@gmail.com>
2016-02-01 15:48:04 -0500

    save

--- src/solver/module.go
+++ src/solver/module.go
@@ -1,11 +1,11 @@
 package nil
 
-import(
+import (
 	"fmt"
 	c "github.com/skilstak/go/colors"
 	i "input"
+	s "other"
 	"strings"
-	"other"
 )
 
 //////////////////////////////////////////////////
@@ -13,10 +13,16 @@ import(
 //The real programming code is in another castle//
 //////////////////////////////////////////////////
 
-
-func Startup(){
+func Startup() {
 	//the only function in this
 	isDone := false
-	for isDone == false{
+	for isDone == false {
 		//So that you don't have to restart the program each time
+		fmt.Println(c.CL + c.B00 + "Mathmatic solver program thing.")
+		s.Spacer(2)
+		//Adds two lines
+		fmt.Println(c.B1 + "What are you too lazy to do today?")
+		fmt.Println(c.Y + "Select one of the letters in red")
+		s.Spacer(1)
 	}
+}
