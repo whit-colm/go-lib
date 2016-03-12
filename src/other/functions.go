02026537c74708f6434819b282ca891d935a323e
Whitman Huntley <whitman-colm@users.noreply.github.com>
2016-03-11 20:19:49 -0500

    Update functions.go

--- src/other/functions.go
+++ src/other/functions.go
@@ -57,7 +57,7 @@ func QuitAtError(problem error) {
 		fmt.Println(errorResponse[m.Intn(9)])
 		fmt.Println()
 		fmt.Println(c.V + "Details of the error are below:" + c.B00)
-		fmt.Printf("%s%v%s\n",c.B01,problem,c.BX)
+		fmt.Printf("%s%v%s\n",c.B01,problem,c.X)
 		os.Exit(1)
 	} else {
 		fmt.Println(c.V + "ERROR CHECK: " + c.B00 + "no errors found")
